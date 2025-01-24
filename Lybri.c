53f5ef8b5e7e41b06cbfe5d2b0d37981e3710556
Aichatou-Malam <aichatoumalam63@gmail.com>
2025-01-24 21:44:08 +0100

    Update Lybri.c

--- Lybri.c
+++ Lybri.c
@@ -1,5 +1,5 @@
 #include "Lybri.h" 
-// Définition d'une fonction de calcul de factorielle. 
+// DÃ©finition d'une fonction de calcul de factorielle. 
 unsigned int fact( unsigned int value ) { 
 unsigned int result = 1; 
 while ( value > 1 ) { 
@@ -7,7 +7,7 @@ result *= value;
 value --; }
 return result;
  } 
- // Définition d'une fonction d'élévation à une puissance données. 
+ // DÃ©finition d'une fonction d'Ã©lÃ©vation Ã  une puissance donnÃ©es. 
  int power( int value, unsigned int pow ) { 
  if ( pow == 0 ) return 1; 
  if ( pow == 1 ) return value;
@@ -17,4 +17,64 @@ accumulator *= value; pow--;
 } 
 return accumulator; 
 }
+// Exercice 1 affichage du message Bonjour.
+// Exercice 2 lecture d'un entier a 03 chiffres et afficher son inverse.
+void inverse() {
+int nombre,inverse=0;
+scanf("%d", &nombre);
+if(nombre < 100|| nombre > 999) {
+    printf("Entrer un nombre valide Ã  trois chiffres.\n");
+}
+while (nombre !=0)
+{
+  int chiffre = nombre%10;
+  inverse = inverse*10+chiffre;
+  nombre /=10;
+}
+printf("le nombre inverse est : %d\n",  inverse");
+}
+// Exercice 3 lecture de deux nombres reels et affichage de leur produit.
+void produit() {
+  float x, y, produit;
+  printf("Entrer deux nombres reels : ");
+  scanf("%f%f", &x, &y);
+   produit = x * y;
+printf("le produit des deux reels est :  %.3f\n", produit);
+}
+// Exercice 4 paritÃ© 
+void pairouimpair() {
+   int N;
+   printf("Entrer un entier : ");
+   scanf("%d", &N);
+   if (N%2 == 0) {
+       printf("%d est pair", N);
+   }
+   else {
+       printf("%d est impair", N);
+   }
+}
+// Exercice 5 comparaison de deux entiers.
+void comparaison() {
+     int a,b;
+     printf("Entrer deux entiers : ");
+     scanf("%d%d", &a , &b);
+     if(a == b)
+     {
+       printf("%d=%d\n", a,b);
+     }
+     else {
+     if(a>b)
+     {
+      printf("%d>%d\n", a,b);
+     }
+     else
+     {
+       printf("%d<%d\n", a,b);
+     }
+ }
+}
+// Exercice 6 affichage dans un ordre croissant 03 entiers.
+void ordrecroissant() {
+      int a,b,c;
 
+  
