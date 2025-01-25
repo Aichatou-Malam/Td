1d462e38982d1432124fda69b05877cc4e9c3d7b
Aichatou-Malam <aichatoumalam63@gmail.com>
2025-01-25 16:59:30 +0100

    Update Lybri.c

--- Lybri.c
+++ Lybri.c
@@ -76,5 +76,32 @@ void comparaison() {
 // Exercice 6 affichage dans un ordre croissant 03 entiers.
 void ordrecroissant() {
       int a,b,c;
-
+   printf("entrer trois entiers : ");
+   scanf("%d%d%d", &a, &b, &c);
+        if (a>b && a>c) {
+            if (b>c)
+   printf("%d<%d<%d\n", c,b,a );
+                   else printf("%d<%d<%d", b,c,a);
+ }
+  if (b>a && b>c) {
+          if(a>c)
+  printf("%d<%d<%d\n", c,a,b);
+  else printf("%d<%d<%d", a,c,b);
+  } 
+  if (c>a && c>b) { 
+      if (a>b)
+   printf("%d<%d<%d", b,a,c);
+    else printf ("%d<%d<%d", a,b,c);
+  }
+  } 
+//Exercice 16 permutation
+void permutation_char(char* a, char* b ) {
+        char temp = *a;
+          *a = *b;
+          *b = temp;
+}
+//Exercice 17         
+int Somme(int a, int b)  {
+        return a + b;
+}
   
